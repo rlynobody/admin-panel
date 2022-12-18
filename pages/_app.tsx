ac59d015f3bac790ecceb7a96f64a0f725abc00e
rlynobody <sadexitus@gmail.com>
2022-12-18 17:56:42 +0700

    Base structure

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app';
+
+import '../app/assets/styles/globals.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
